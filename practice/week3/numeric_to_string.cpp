286cc3eba3f6af1497da65e479dff7bb683e9c9a
wlstjd-art <wlstjdznf6@gmail.com>
2026-09-21 15:17:53 +0900

    cc

--- practice/week3/numeric_to_string.cpp
+++ practice/week3/numeric_to_string.cpp
@@ -4,11 +4,13 @@
 int main(){
 
 
-    std::string s1 = "apple";
+    std::string s1 = "appleA";
     std::string s2;
 
     s2 = s1 + " "+ std::to_string(10);
-    std::cout <<s2<<'\n';
+    std::cout <<s2  <<'\n';
+
+
 
 
 
